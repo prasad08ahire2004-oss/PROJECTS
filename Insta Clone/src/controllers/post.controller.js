0c3de2df6db5fb8f069ca3f623729ea993e958e4
Prasad Ahire <prasad08ahire2004@gmail.com>
2026-09-14 20:47:16 +0530

    sixth commit- created getpostcontroller and getpostdetailscontroller API

--- Insta Clone/src/controllers/post.controller.js
+++ Insta Clone/src/controllers/post.controller.js
@@ -1,6 +1,8 @@
 const postModel = require('../models/post.model')
 const ImageKit = require('@imagekit/nodejs')
 const { toFile } = require('@imagekit/nodejs')
+const { Folders } = require('@imagekit/nodejs/resources/index.js')
+const jwt = require('jsonwebtoken')
 
 const imageKit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
@@ -9,14 +11,136 @@ const imageKit = new ImageKit({
 async function createPostController(req,res) {
     console.log(req.body, req.file)
 
+    // yaha hum ye pata karenge ki konsa user post kar raha hai, to hum token ko verify karenge aur user ka id nikalenge
+
+    const token = req.cookies.token
+
+    if(!token) {
+        return res.status(401).json({
+            message:"token not provided unauthorized access"
+        })
+    }
+
+    let decoded = null
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message:"invalid token unauthorized access"
+        })
+    }
+
+    console.log(decoded)
+
     const file = await imageKit.files.upload({
         file: await toFile(req.file.buffer, req.file.originalname),
-        fileName: "Test"
+        fileName: "Test",
+        folder: "/insta-clone"
+        // jitni bhi images imagekit par dalenge vo sari is folder par store honge jo imagekit ke website par present hoga inside [media library] section
     })
 
-    res.send(file)
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+
+    res.status(201).json({
+        message: "Post created successfully",
+        post
+    })
+}
+
+async function getPostController(req,res) {
+    const token = req.cookies.token
+
+    if(!token) {
+        return res.status(401).json({
+            message:"token not provided unauthorized access"
+        })
+    }
+
+    let decodec = null
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message:"invalid token unauthorized access"
+        })
+    }
+
+    //yaha hum vo sare post ko return karwa rahe hai jo post is user ne create ki hai jise hum ne decode karwaya hai
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message:"posts fetched successfully",
+        posts
+    })
+}
+
+async function getPostDetailsController(req,res) {
+
+    const token = req.cookies.token
+
+    if(!token) {
+        return res.status(401).json({
+            message:"token not provided unauthorized access"
+        })
+    }
+
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message:"invalid token unauthorized access"
+        })
+    }
+
+    const userId = decoded.id
+
+    const postId = req.params.postId
+
+    const post = await postModel.findOne({
+        _id: postId,
+        user: userId
+    })
+
+    if(!post) {
+        return res.status(404).json({
+            message:"post not found"
+        })
+    }
+
+    res.status(200).json({
+        message:"post details fetched successfully",
+        post
+    })
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser) {
+        return res.status(403).json({
+            message:"you are not authorized to view this post"
+        })
+    }
+
+    return res.status(200).json({
+        message:"post details fetched successfully",
+        post
+    })
 }
 
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
--- Insta Clone/src/routes/post.routes.js
+++ Insta Clone/src/routes/post.routes.js
@@ -4,7 +4,18 @@ const postController = require('../controllers/post.controller')
 const multer = require('multer')
 const upload = multer({storage: multer.memoryStorage()})
 
+
+//is API mai phale hum token ki help se konse user ne post create karne ki request dali hai vo pata kiya aur post bani
 postRouter.post("/" , upload.single('image') , postController.createPostController)
+
+
+//is API ke andar hum ne jo bhi user ne request dali hai usko token ki help se verify kiya aur us user ki sari posts ko humne database se fetch kiya aur user ko send kiya
+postRouter.get("/" , postController.getPostController)
+
+
+//is API ke andar hum post id ki help se humne database se post ki details ko fetch kiya aur user ko send kiya
+postRouter.get("/details/:postId" , postController.getPostDetailsController)
+
 
 
 module.exports = postRouter
