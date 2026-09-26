83c4f02ea52f6899aacdb6c1908f4c01abde1e17
Prasad Ahire <prasad08ahire2004@gmail.com>
2026-09-26 22:43:09 +0530

    tewnty two commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import AppRoutes from './APPRoutes.jsx'
 import './style.scss'
+import { AuthProvider } from './features/auth/auth.context.jsx'  
 
 const App = () => {
   return (
-    <AppRoutes />
+    <AuthProvider>
+
+      <AppRoutes />
+
+    </AuthProvider>
+    
   )
 }
 
--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,24 +1,21 @@
 import React , {useState} from 'react'
 import '../style/form.scss'
 import {Link} from 'react-router-dom'
-import axios from 'axios'
+import { useAuth } from '../hooks/useAuth'
 
 const Login = () => {
 
   const [username , setUsername] = useState("")
   const [password , setPassword] = useState("")
 
+  const {handleLogin} = useAuth()
+
   function handleSubmit(e){
     e.preventDefault()
 
-    axios.post('http://localhost:3000/api/auth/login' , {
-      username,
-      password,
-    },{
-      withCredentials:true
-    })
+    handleLogin(username , password)
     .then(res => {
-      console.log(res.data)
+      console.log(res)
     })
   }
 
